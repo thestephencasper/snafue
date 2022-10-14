51fd6e35d8ba09dcd66488a9a3da64a41e0721d4
casper <thestephencasper@gmail.com>
2022-10-13 23:26:16 -0400

    fixed bug

--- download_data.sh
+++ download_data.sh
@@ -11,6 +11,7 @@ wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar -q --show-pro
 mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
 wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
 echo 'imagenet validation set done...'
+cd ../..
 
 # broden
 # https://github.com/davidbau/quick-netdissect
